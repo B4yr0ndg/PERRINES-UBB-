61bfb2b1687d5dd5ce3f94b63adde558c7e889c0
B4yr0ndg <141662151+B4yr0ndg@users.noreply.github.com>
2024-04-18 00:00:01 -0400

    Delete alert.route.js

--- backend/src/routes/alert.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const alertController = require("../controllers/alert.controller");
-
-router.post("/", alertController.createAlert);
-router.get("/", alertController.getAllAlerts);
-router.put("/", alertController.updateAlert);
-
-module.exports = router;
