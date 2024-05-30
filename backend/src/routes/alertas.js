a0a7a5b68802dbbebab152172d6871c207fd67ed
JesuuToledo <162846437+JesuuToledo@users.noreply.github.com>
2024-05-29 21:20:41 -0400

    Update alertas.js

--- backend/src/routes/alertas.js
+++ backend/src/routes/alertas.js
@@ -1,14 +1,16 @@
 // Se importa el modulo
-    const express = require("express");
+    import express from "express";
 // Se crea un enrutador 
     const router = express.Router();
 // Se importa el controlador
-    const alertController = require("../controllers/alertas.controller");
+    import alertController from "../controllers/alertas.controller";
 // Se definen las rutas y se asocian a las funciones del controlador
+
     router.get('/', alertasController.getAllAlertas);
     router.get('/:id', alertasController.getAlertaById);
     router.post('/', alertasController.createAlerta);
     router.put('/:id', alertasController.updateAlerta);
     router.delete('/:id', alertasController.deleteAlerta);
+
 // se exporta el enrutador 
-    module.exports = router;
+    export default router;
