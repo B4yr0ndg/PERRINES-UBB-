161bf15b181364e2f913f6fcc1d60ff1abc37f03
B4yr0ndg <141662151+B4yr0ndg@users.noreply.github.com>
2024-05-31 11:01:02 -0400

    Delete backend directory

--- backend/src/utils/scheduler.js
+++
@@ -1,8 +0,0 @@
-const cron = require("node-cron");
-const notificationService = require("../services/notification.service");
-
-cron.schedule("*/10 * * * *", () => {
-    // eslint-disable-next-line no-console
-    console.log("Checking for scheduled alerts to send...");
-    notificationService.checkAndSendAlerts();
-});
