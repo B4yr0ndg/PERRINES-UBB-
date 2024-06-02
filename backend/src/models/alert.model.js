566e6d60ebbf22efd4e447bede0e0f11d8b92af9
B4yr0ndg <141662151+B4yr0ndg@users.noreply.github.com>
2024-06-02 00:00:57 -0400

    Update alert.model.js

--- backend/src/models/alert.model.js
+++ backend/src/models/alert.model.js
@@ -1,43 +1,42 @@
 // SE IMPORTA LA BIBLIOTECA DE MONGOOSE
-import mongoose from ¨mongoose¨;
+import mongoose from "mongoose";
 
 // SE DEFINE EL ESQUEMA DE LA COLECCION DE NOTIFICACIONES
 const alertSchema = new mongoose.Schema({
-// NOMBRE DE LA NOTIFICACION 
-        titulo: {
+    // NOMBRE DE LA NOTIFICACION 
+    titulo: {
         type: String,
         enum: ["appointment", "task"],
         required: true,
     },
-        
-// PRIORIDAD DE LA NOTIFICION 
+    // PRIORIDAD DE LA NOTIFICION 
     prioridad: {
         type: String,
         enum: ["alta", "media", "baja"],
         required: true,
     },
-
-// CONTENIDO DEL MENSAJE
+    // CONTENIDO DEL MENSAJE
     mensaje: {
         type: String,
         required: true,
     },
-// SE ALMACENA EL EMAIL PARA EL ENVIO DE LA NOTIFICACION 
-    Email: {
+    // SE ALMACENA EL EMAIL PARA EL ENVIO DE LA NOTIFICACION 
+    email: {
         type: String,
         required: true,
     },
-// EN ESTA PARTE SE ALMACENA EL EMAIL, PARA EL ENVIO DE LA SIGUIENTE NOTIFICACION
+    // EN ESTA PARTE SE ALMACENA EL EMAIL, PARA EL ENVIO DE LA SIGUIENTE NOTIFICACION
     nextSendDate: {
         type: Date,
         required: true,
     },
-// SE ESTABLECE UN INTERVALO HORARIO DE LAS NOTIFICACIONES
+    // SE ESTABLECE UN INTERVALO HORARIO DE LAS NOTIFICACIONES
     interval: { // Intervalo en horas
         type: Number,
         required: true,
-    }, 
-    { timestamps: true });
-});
+    },
+}, { timestamps: true });
 
+// SE EXPORTA EL MODELO DE LA NOTIFICACION
 export default mongoose.model("Alert", alertSchema);
+
